ede90db88c8440e8ce2ee7389c959e77f7c13528
prasannac-cuelogic <prasanna.champanerkar@cuelogic.co.in>
2015-03-12 12:12:32 +0530

    traning symfoony project added

---
+++ src/Ens/JobeetBundle/Controller/CategoryController.php
@@ -0,0 +1,13 @@
+<?php
+namespace Ens\JobeetBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ens\JobeetBundle\Entity\Category;
+
+/**
+* Category controller.
+*
+*/
+class CategoryController extends Controller
+{
+
+}
